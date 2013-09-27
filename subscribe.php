ccbd5e514911afecf16361bbaa4c9510fdf5f82b
maricn <maricn@gmail.com>
2013-09-27 18:19:41 +0200

    subscribe and unsubscribe should work now (for real)

--- subscribe.php
+++ subscribe.php
@@ -1,29 +1,29 @@
 <?php
-    $jsonString = file_get_contents('php://input');
-    $json = json_decode($jsonString, false);
-    
-    $deviceId = $json->{'device_id'};
-    $notificationUrl = "";
-    if(array_key_exists('push_url', $json)) {
-        $notificationUrl = $json->{'push_url'};
-    }
-    
-    echo $jsonString . "\n" . $deviceId . "\n" . $notificationUrl;
-    
-    require_once './db/connect.php';
-    
-    $db = dbConnect();
-    $query = sprintf("INSERT INTO `badgermm_infobip`.`user` (`userid`, `url`)
-                      VALUES ('%s', '%s')
-                      ON DUPLICATE KEY UPDATE
-                      url='%s'",
-            mysql_real_escape_string($deviceId),
-            mysql_real_escape_string($notificationUrl),
-            mysql_real_escape_string($deviceId)
-    );
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $jsonString = file_get_contents('php://input');
+        $json = json_decode($jsonString, false);
 
-    $result = mysql_query($query, $db);
-    if (!$result) {
-        throw new Exception('Could not enter data: '. mysql_error());
+        $deviceId = $json->{'device_id'};
+        $notificationUrl = "";
+        if(array_key_exists('push_url', $json)) {
+            $notificationUrl = $json->{'push_url'};
+        }
+
+        require_once './db/connect.php';
+
+        $db = dbConnect();
+        $query = sprintf("INSERT INTO `badgermm_infobip`.`user` (`userid`, `url`)
+                          VALUES ('%s', '%s')
+                          ON DUPLICATE KEY UPDATE
+                          url = '%s'",
+                mysql_real_escape_string($deviceId),
+                mysql_real_escape_string($notificationUrl),
+                mysql_real_escape_string($notificationUrl)
+        );
+
+        $result = mysql_query($query, $db);
+        if (!$result) {
+            throw new Exception('Could not enter data: '. mysql_error());
+        }
     }
 ?>
--- unsubscribe.php
+++ unsubscribe.php
@@ -1,16 +1,19 @@
 <?php
-    $json = json_decode($_POST, true);
-    $deviceId = $json["device_id"];
-    
-    require_once './db/connect.php';
-    
-    $db = dbConnect();
-    $query = sprintf("DELETE FROM `badgermm_infobip`.`user` WHERE `userid` = '%s'",
-            mysql_real_escape_string($deviceId)
-    );
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $jsonString = file_get_contents('php://input');
+        $json = json_decode($jsonString, false);
+        $deviceId = $json->{'device_id'};
+        
+        require_once './db/connect.php';
 
-    $result = mysql_query($query, $db);
-    if (!$result) {
-        throw new Exception('Could not delete data: '. mysql_error());
+        $db = dbConnect();
+        $query = sprintf("DELETE FROM `badgermm_infobip`.`user` WHERE `userid` = '%s'",
+                mysql_real_escape_string($deviceId)
+        );
+
+        $result = mysql_query($query, $db);
+        if (!$result) {
+            throw new Exception('Could not delete data: '. mysql_error());
+        }
     }
 ?>
